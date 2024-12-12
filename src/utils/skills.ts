99df4f07614510d0dc22b4bd12d99bafe625477f
Victoria Cheng <victoria.cheng15@gmail.com>
2024-12-12 13:34:04 -0700

    add skills (#4)
    * chore: add skill icons
    
    * feat: format skill name for file path
    
    * feat: skill component
    
    * feat: add skill component to homepage

---
+++ src/utils/skills.ts
@@ -0,0 +1,36 @@
+const SKILLS = [
+  "TypeScript",
+  "JavaScript",
+  "Python",
+  ".NET",
+  "HTML",
+  "CSS",
+  "React",
+  "Next.js",
+  "Redux",
+  "Tailwind CSS",
+  "SASS",
+  "Node.js",
+  "Express",
+  "Flask",
+  "MongoDB",
+  "PostgreSQL",
+  "Cypress",
+  "Jest",
+  "Testing Library",
+  "Github Actions",
+  "Docker",
+  "Bash",
+  "Linux",
+];
+
+function formatForFilePath(skills: string[]): string[] {
+  return skills.reduce((acc: string[], curr: string) => {
+    const formattedString = curr.replace(/\./g, "dot").replace(/\s+/g, "").toLowerCase()
+    acc.push(formattedString)
+
+    return acc
+  }, []);
+}
+
+export const formatSkillNames = formatForFilePath(SKILLS)
