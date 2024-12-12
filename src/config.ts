7256808126c5db3d13db01ba8c74690da0e8f27c
Victoria Cheng <victoria.cheng15@gmail.com>
2024-12-12 14:29:58 -0700

    add foramtters (#6)
    * feat: add biomejs and prettier
    
    * chore: add config for biome js and prettier
    
    * chore: format files

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,24 @@
 import type { PostFilter } from "./utils/posts";
 
 export interface SiteConfig {
-  title: string;
-  slogan: string;
-  description?: string;
-  site: string,
-  rss?: boolean;
-  homepage: PostFilter;
+	title: string;
+	slogan: string;
+	description?: string;
+	site: string;
+	rss?: boolean;
+	homepage: PostFilter;
 }
 
 export const siteConfig: SiteConfig = {
-  site: "https://example.com/",
-  title: "Victoria",
-  slogan: "Navigating the endless world of technology.",
-  description: "Welcome to my tech hub! Explore my blogs, projects, and skills as I navigate the ever-evolving world of technology. Join me on this journey of discovery and innovation.",
-  rss: true,
-  homepage: {
-    maxPosts: 5,
-    tags: [],
-    excludeTags: [],
-  }
+	site: "https://example.com/",
+	title: "Victoria",
+	slogan: "Navigating the endless world of technology.",
+	description:
+		"Welcome to my tech hub! Explore my blogs, projects, and skills as I navigate the ever-evolving world of technology. Join me on this journey of discovery and innovation.",
+	rss: true,
+	homepage: {
+		maxPosts: 5,
+		tags: [],
+		excludeTags: [],
+	},
 };
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { defineCollection, z } from 'astro:content';
-import {glob} from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ base: "./src/content/blog", pattern: '**/*.md' }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    date: z.date(),
-    tags: z.array(z.string()).optional(),
-    draft: z.boolean().optional(),
-    image: z.string().optional(),
-  }),
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string().optional(),
+		date: z.date(),
+		tags: z.array(z.string()).optional(),
+		draft: z.boolean().optional(),
+		image: z.string().optional(),
+	}),
 });
 
 export const collections = { blog };
