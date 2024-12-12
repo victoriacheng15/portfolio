73ac06602e5f94beeafb931b0345e6c9fce99144
Victoria Cheng <victoria.cheng15@gmail.com>
2024-12-12 14:29:44 -0700

    chore: format files

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -1,36 +1,39 @@
 const SKILLS = [
-  "TypeScript",
-  "JavaScript",
-  "Python",
-  ".NET",
-  "HTML",
-  "CSS",
-  "React",
-  "Next.js",
-  "Redux",
-  "Tailwind CSS",
-  "SASS",
-  "Node.js",
-  "Express",
-  "Flask",
-  "MongoDB",
-  "PostgreSQL",
-  "Cypress",
-  "Jest",
-  "Testing Library",
-  "Github Actions",
-  "Docker",
-  "Bash",
-  "Linux",
+	"TypeScript",
+	"JavaScript",
+	"Python",
+	".NET",
+	"HTML",
+	"CSS",
+	"React",
+	"Next.js",
+	"Redux",
+	"Tailwind CSS",
+	"SASS",
+	"Node.js",
+	"Express",
+	"Flask",
+	"MongoDB",
+	"PostgreSQL",
+	"Cypress",
+	"Jest",
+	"Testing Library",
+	"Github Actions",
+	"Docker",
+	"Bash",
+	"Linux",
 ];
 
 function formatForFilePath(skills: string[]): string[] {
-  return skills.reduce((acc: string[], curr: string) => {
-    const formattedString = curr.replace(/\./g, "dot").replace(/\s+/g, "").toLowerCase()
-    acc.push(formattedString)
+	return skills.reduce((acc: string[], curr: string) => {
+		const formattedString = curr
+			.replace(/\./g, "dot")
+			.replace(/\s+/g, "")
+			.toLowerCase();
+		acc.push(formattedString);
 
-    return acc
-  }, []);
+		return acc;
+	}, []);
 }
 
-export const formatSkillNames = formatForFilePath(SKILLS)
+export const formatSkillNames = formatForFilePath(SKILLS);
