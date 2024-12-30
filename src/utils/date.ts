9a6ca019becb5f87368b3811ba37e7f34f16fb85
Victoria Cheng <victoria.cheng15@gmail.com>
2024-12-30 13:28:52 -0700

    fix: display date as the publish date (#23)
    * fix: display date as the publish date
    
    * style: files have formatted!
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,14 @@
 export const currentYear = new Date().getFullYear();
 
 export function formatDate(date: Date): string {
-	return date.toLocaleDateString("en-US", {
-		year: "numeric",
+	const month = date.toLocaleString("en-US", {
 		month: "long",
-		day: "numeric",
+		timeZone: "UTC",
 	});
+	const day = date.getUTCDate();
+	const year = date.getUTCFullYear();
+
+	return `${month} ${day}, ${year}`;
 }
 
 export function isPublished(date: Date): boolean {
