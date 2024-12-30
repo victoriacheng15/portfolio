52e76cb0c5e27d2cea20c955fab2a0d1007547f6
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2024-12-30 20:27:46 +0000

    style: files have formatted!

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,14 @@
 export const currentYear = new Date().getFullYear();
 
 export function formatDate(date: Date): string {
-  const month = date.toLocaleString("en-US", { month: "long", timeZone: "UTC" });
-  const day = date.getUTCDate();
-  const year = date.getUTCFullYear();
+	const month = date.toLocaleString("en-US", {
+		month: "long",
+		timeZone: "UTC",
+	});
+	const day = date.getUTCDate();
+	const year = date.getUTCFullYear();
 
-  return `${month} ${day}, ${year}`;
+	return `${month} ${day}, ${year}`;
 }
 
 export function isPublished(date: Date): boolean {
