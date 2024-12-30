af68f5b3e7841672b702c20c6aadd667dcb89c97
Victoria Cheng <victoria.cheng15@gmail.com>
2024-12-30 13:58:26 -0700

    feat: add setup post and publish post scripts

---
+++ scripts/publish_post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "src/content/blog" || exit
+
+tomorrow=$(date -d "tomorrow" +%Y-%m-%d)
+
+for file in *.md; do
+
+  file_date=$(grep '^date:' "$file" | awk '{print $2}' | tr -d '"')
+
+  if [[ "$file_date" == "$tomorrow" ]]; then
+    echo "Processing $file..."
+
+    sed -i '/^draft:/d' "$file"
+  fi
+
+done
---
+++ scripts/setup_post.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Enter the name of the post branch: "
+read -r post_branch
+
+if [[ -n "$post_branch" ]]; then
+  git switch -c post/"$post_branch"
+else
+  echo "no input provided, skipping the git branch command."
+fi
+
+cd src/content/blog || exit
+
+echo "Enter the title of the post: "
+read -r title
+echo "Enter the description of the post: "
+read -r description
+echo "Enter tags: "
+read -r tags
+echo "Enter month: "
+read -r mm
+echo "Enter day: "
+read -r dd
+
+slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr -d '-' | sed 's/ \{1,\}/ /g' | tr -d ':' | sed 's/ \+/-/g')
+
+year=$(date +%Y)
+month=${mm:-$(date +%m)}
+day=${dd:-$(date +%d)}
+
+date=$year-$month-$day
+
+touch $slug.md
+echo "https://victoriacheng15.vercel.app/posts/$slug"
+
+
+cat <<EOF > $slug.md
+---
+title: "$title"
+description: "$description"
+date: $date
+tags: [$tags]
+draft: true
+---
+EOF
