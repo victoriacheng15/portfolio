d6ea5a16ce9cb6917ad97ba1e17ea085f9b25c8f
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-08 13:59:18 -0700

    chore: ensure the date is in UTC (#70)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,14 +1,12 @@
 export const currentYear = new Date().getFullYear();
 
 export function formatDate(date: Date): string {
-	const month = date.toLocaleString("en-US", {
+	return date.toLocaleDateString("en-US", {
+		year: "numeric",
 		month: "long",
+		day: "numeric",
 		timeZone: "UTC",
 	});
-	const day = date.getUTCDate();
-	const year = date.getUTCFullYear();
-
-	return `${month} ${day}, ${year}`;
 }
 
 export function isPublished(date: Date): boolean {
