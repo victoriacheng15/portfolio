dc28456dc1f302a44a71aa14a5a2858e5c12dbde
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-18 10:05:44 -0700

    chore: add twilind css plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,29 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import rehypePrettyCode from 'rehype-pretty-code';
-import { siteConfig } from './src/config';
+import { defineConfig } from "astro/config";
+import rehypePrettyCode from "rehype-pretty-code";
+import { siteConfig } from "./src/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: siteConfig.site,
-  integrations: [tailwind()],
-  markdown: {
-    rehypePlugins: [
-      [rehypePrettyCode, {
-        theme: 'github-dark',
-        onVisitLine(node) {
-          if (node.children.length === 0) {
-            node.children = [{type: 'text', value: ' '}];
-          }
-        },
-      }],
-    ],
-  },
+	site: siteConfig.site,
+	integrations: [],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypePrettyCode,
+				{
+					theme: "github-dark",
+					onVisitLine(node) {
+						if (node.children.length === 0) {
+							node.children = [{ type: "text", value: " " }];
+						}
+					},
+				},
+			],
+		],
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
